ef90689e813f26e6b4888b7e4752f4a3270d8424
kayutenko <kayutenko@mail.ru>
2022-05-03 14:15:12 +0300

    Update yourpass.js

--- scripts/yourpass.js
+++ scripts/yourpass.js
@@ -22,7 +22,8 @@ async function createPass() {
     method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
+    mode: 'no-cors'
   };
 
   var response = await fetch("https://yi8vccgkxg.execute-api.us-east-2.amazonaws.com/default/YourPass_CreatePass", requestOptions)
